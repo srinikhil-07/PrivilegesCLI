e8f3fa41ffc879e1606a04298ecb10b77b30653d
Nikhil0487 <nikhilduggirala95@gmail.com>
2020-03-22 16:05:54 +0530

    Privileges: Added Swift argument parser.
    This commit if included will:
    1. Added Swift argument parser to get input for the CLI,
    2. User name and admin needed options were added.

--- privilege/main.swift
+++ privilege/main.swift
@@ -7,19 +7,24 @@
 //
 
 import Foundation
-import com_privilege_helper
+import ArgumentParser
 
-
-
-print("Hello, World!")
-
-let connection = NSXPCConnection(machServiceName: "com.privilege.helper", options: .privileged)
-connection.remoteObjectInterface = NSXPCInterface(with: ListenerProtocol.self)
-connection.resume()
-let service = connection.remoteObjectProxyWithErrorHandler { error in
-    print("Error if any: \(error)")
-} as? ListenerProtocol
-service?.changePrivilege(for: "Test", toAdmin: false)
-service?.upperCaseString("hello XPC") { response in
-    print("Response from XPC service:", response)
+struct Privilege: ParsableCommand {
+    @Option(name: .shortAndLong, default: "Test", help: "The user whose privlige needs change")
+    var user: String
+    @Option(name: .shortAndLong, default: true, help: "Needs admin privilege")
+    var admin: Bool
+    func run() throws {
+        let connection = NSXPCConnection(machServiceName: "com.privilege.helper", options: .privileged)
+        connection.remoteObjectInterface = NSXPCInterface(with: ListenerProtocol.self)
+        connection.resume()
+        let service = connection.remoteObjectProxyWithErrorHandler { error in
+            print("Error if any: \(error)")
+        } as? ListenerProtocol
+        service?.changePrivilege(for: user, toAdmin: admin)
+        service?.upperCaseString("hello XPC") { response in
+            print("Response from XPC service:", response)
+        }
+    }
 }
+Privilege.main()
